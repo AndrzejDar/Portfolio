bfaa56daa708581b4d8e07c179c7152fc3e8d727
Andrzej Darowski <andrzej.dar@gmail.com>
2022-06-23 13:25:18 +0200

    WIP: 3 first sections

---
+++ src/container/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import {images} from '../../constants';
+import {AppWrap, MotionWrap} from '../../wrapper';
+import {client} from '../../client';
+
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <>
+       <h2 className='head-text'>zporttt</h2>
+       <div className='app__footer-cards'>
+        <div className='app__footer-card'>
+          <img src={images.email} alt='email' />
+          <a href='mailto:andrzej.dar@gmail.com' className="p-text" />
+        </div>
+        <div className='app__footer-card'>
+          <img src={images.mobile} alt='mobile' />
+          <a href='tel: +48515066320' className="p-text" />
+        </div>
+
+       </div>
+    </>
+  );
+}
+
+export default AppWrap( 
+  MotionWrap (Footer, 'app__footer'),
+  'contact',
+  'app__whitebg'
+) 
